0c2802ec730903954cb5063041459d060bdcc68d
kyrow <mr.khan4ik@gmail.com>
2023-12-20 16:54:23 +0600

    added filter to fields

--- src/shared/store/slices/usersList-slice.ts
+++ src/shared/store/slices/usersList-slice.ts
@@ -10,7 +10,14 @@ const userListSlice = createSlice({
 	name: 'userList',
 	initialState,
 	reducers: {
-
+		searchUser: (state, action) => {
+			state.users = state.users.filter(user =>
+				user.firstName.toLowerCase().includes(action.payload) ||
+				user.lastName.toLowerCase().includes(action.payload) ||
+				user.email.toLowerCase().includes(action.payload) ||
+				user.phone.includes(action.payload)
+			);
+		}
 	},
 	extraReducers: (builder) => {
 		builder.addCase(getUserList.fulfilled, (state, action) => {
@@ -19,5 +26,5 @@ const userListSlice = createSlice({
 	},
 })
 
-// export const { } = userListSlice.actions
+export const { searchUser } = userListSlice.actions
 export default userListSlice.reducer
--- src/widgets/userList/index.tsx
+++ src/widgets/userList/index.tsx
@@ -1,32 +1,58 @@
-import { Table } from "antd"
+import { Button, Flex, Table } from "antd"
 import { fieldsTitle } from "../../features/fieldsControl"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getUserList } from "../../shared/store/slices/getUserList"
 import { useDispatch, useSelector } from "react-redux"
 import { AppDispatch, RootState } from "../../shared/store/store"
-
+import Search from "antd/es/input/Search"
+import { searchUser } from "../../shared/store/slices/usersList-slice"
 
 
 function UserListWidget() {
 
 	const dispatch = useDispatch<AppDispatch>()
 	const { users } = useSelector((state: RootState) => state.userListReducer);
-
+	const [isSearchMode, setSearchMode] = useState(false)
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const fetchData = async () => {
+			setLoading(true);
 			await dispatch(getUserList());
+			setLoading(false);
 		}
 		fetchData();
 	}, [dispatch]);
 
 	return (
-		<Table
-			columns={fieldsTitle}
-			dataSource={users}
-			pagination={{ pageSize: 50, position: ['topRight'] }}
-			scroll={{ y: "80vh" }}
-		/>
+		<div style={{ height: '100vh', position: 'relative' }}>
+			<Flex style={{ position: 'absolute', top: 16, left: 0, width: 300, zIndex: 5 }} gap='small'>
+				<Search
+					placeholder="find a user"
+					onSearch={(value: string) => {
+						setLoading(true);
+						dispatch(searchUser(value.toLowerCase()))
+						setLoading(false);
+						setSearchMode(true)
+					}}
+					enterButton
+				/>
+				{isSearchMode && <Button onClick={() => {
+					setLoading(true);
+					dispatch(getUserList())
+					setLoading(false);
+					setSearchMode(false)
+				}}>Reset</Button>}
+			</Flex>
+
+			<Table
+				columns={fieldsTitle}
+				dataSource={users}
+				pagination={{ pageSize: 50, position: ['topRight'] }}
+				loading={loading}
+				scroll={{ y: "80vh" }}
+			/>
+		</div>
 	)
 }
 
